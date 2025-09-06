1fdaa88759cc5a03fcfdfb984c6202e3d2bafd17
CMGSK@main <carlosmgov7k@gmail.com>
2025-09-06 16:31:34 +0200

    first commit

---
+++ src/result/err.py
@@ -0,0 +1,53 @@
+from typing import NoReturn
+from result.interface import E, F, T, U, Result
+
+class Err(Result[NoReturn, E]):
+    """
+    Represents a failed operation from a Result returning 
+    function containing the value E.
+
+    Success type is NoReturn because an Err cannot (and should not
+    be forced to) contain a non-error type.
+    """
+
+    def __init__(self, error):
+        self._error = error
+
+    def __repr__(self):
+        return f'Err({self._error!r})'
+
+    def __eq__(self, o: object):
+        if isinstance(o, Err):
+            return self._error == o._error
+        return False
+
+
+    def is_ok(self):
+        return False
+
+    def is_err(self):
+        return True
+
+    def unwrap(self):
+        raise self._error
+
+    def expect(self, e):
+        raise RuntimeError(f'{e}.\n{self._error!r}')
+
+    def unwrap_err(self):
+        return self._error
+
+    def unwrap_or(self, fallback): 
+        return fallback
+
+    def map(self, f):
+        return self
+        
+    def map_err(self, f):
+        return Err(f(self._error))
+
+    def and_then(self, f):
+        return self
+
+    def or_else(self, f):
+        return f(self._error)
